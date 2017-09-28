426a9bc029461037b55431143daa189de4914371
Sithara Kalpani <sitharakalpani.sk@gmail.com>
2017-09-28 15:53:51 +0530

    right
    set colors for menus

--- header.php
+++ header.php
@@ -20,6 +20,15 @@
 </head>
 
 
+
+
+
+
+
+
+
+
+
 <body>
     <div id="wrapper">
         <nav class="navbar navbar-default top-navbar" role="navigation">
@@ -33,6 +42,24 @@
                 <a class="navbar-brand" href="index.html">Dream</a>
             </div>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             <ul class="nav navbar-top-links navbar-right">
                 <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
@@ -42,7 +69,7 @@
                         <li>
                             <a href="#">
                                 <div>
-                                    <strong>John Doe</strong>
+                                    <strong>Sunil Premarathne</strong>
                                     <span class="pull-right text-muted">
                                         <em>Today</em>
                                     </span>
@@ -241,65 +268,9 @@
                 </li>
                 <!-- /.dropdown -->
             </ul>
-        </nav>
-        <!--/. NAV TOP  -->
-        <nav class="navbar-default navbar-side" role="navigation">
-            <div class="sidebar-collapse">
-                <ul class="nav" id="main-menu">
-
-                    <li>
-                        <a href="index.php"><i class="fa fa-dashboard"></i> Dashboard</a>
-                    </li>
-                    <li>
-                        <a href="add.php"><i class="fa fa-desktop"></i> Add Students</a>
-                    </li>
-                    <li>
-                        <a href="view.php"><i class="fa fa-bar-chart-o"></i> View Classes</a>
-                    </li>
-                    <li>
-                        <a href="time.php"><i class="fa fa-qrcode"></i> Time table</a>
-                    </li>
-                    
-                    <li>
-                        <a href="vacation.php"><i class="fa fa-table"></i> Vacation time table</a>
-                    </li>
-                    <li>
-                        <a href="form.php"><i class="fa fa-edit"></i> Forms </a>
-                    </li>
 
 
-                    <li>
-                        <a href="#"><i class="fa fa-sitemap"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
-                        <ul class="nav nav-second-level">
-                            <li>
-                                <a href="#">Second Level Link</a>
-                            </li>
-                            <li>
-                                <a href="#">Second Level Link</a>
-                            </li>
-                            <li>
-                                <a href="#">Second Level Link<span class="fa arrow"></span></a>
-                                <ul class="nav nav-third-level">
-                                    <li>
-                                        <a href="#">Third Level Link</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Third Level Link</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Third Level Link</a>
-                                    </li>
 
-                                </ul>
-
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="empty.php"><i class="fa fa-fw fa-file"></i> Empty Page</a>
-                    </li>
-                </ul>
-
-            </div>
 
         </nav>
+        
--- time.php
+++ time.php
@@ -1,6 +1,68 @@
 ﻿<?php include("header.php") ?>
 
+<!--/. NAV TOP  -->
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
 
+                    <li>
+                        <a href="index.php"><i class="fa fa-dashboard"></i> Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="add.php"><i class="fa fa-desktop"></i> Add Students</a>
+                    </li>
+                    <li>
+                        <a href="view.php"><i class="fa fa-bar-chart-o"></i> View Classes</a>
+                    </li>
+                    <li>
+                        <a class="active-menu" href="time.php"><i class="fa fa-qrcode"></i> Time table</a>
+                    </li>
+                    
+                    <li>
+                        <a href="vacation.php"><i class="fa fa-table"></i> Vacation time table</a>
+                    </li>
+                    <li>
+                        <a href="form.php"><i class="fa fa-edit"></i> Forms </a>
+                    </li>
+
+
+                    <li>
+                        <a href="#"><i class="fa fa-sitemap"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
+                        <ul class="nav nav-second-level">
+                            <li>
+                                <a href="#">Second Level Link</a>
+                            </li>
+                            <li>
+                                <a href="#">Second Level Link</a>
+                            </li>
+                            <li>
+                                <a href="#">Second Level Link<span class="fa arrow"></span></a>
+                                <ul class="nav nav-third-level">
+                                    <li>
+                                        <a href="#">Third Level Link</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Third Level Link</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Third Level Link</a>
+                                    </li>
+
+                                </ul>
+
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="empty.php"><i class="fa fa-fw fa-file"></i> Empty Page</a>
+                    </li>
+                </ul>
+
+            </div>
+
+        </nav>
+
+        
 
         <!-- /. NAV SIDE  -->
         <div id="page-wrapper" >
